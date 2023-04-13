a7af8e30577cca9580138f71d19632b4e9ba9b5c
philippeeyraud <philippe.eyraud@ymail.com>
2023-04-13 11:14:39 +0200

    housing

--- src/Pages/Housing/index.js
+++ src/Pages/Housing/index.js
@@ -4,7 +4,9 @@ import Slide from "../../components/Carousel";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import list from "../../Data/index";
-import './index.scss'
+import './index.scss';
+import staractive from '../../assets/staractive .png';
+import starinactive from '../../assets/starinactive .png'
 
 function Housing() {
   const [imageSlide, setImageSlide] = useState([]);
@@ -15,16 +17,45 @@ function Housing() {
     const DataHousing = list.filter((Data) => Data.id === idHousing);
     setImageSlide(DataHousing[0].pictures);
   }, [idHousing]);
-
+    const rating = DataHousing[0].rating;
+    const name = DataHousing[0].host.name.split(' ');
+ 
   return (
     <div>
       <Header />
 
       <Slide imageSlide={imageSlide} />
-      <main class="housing">
-        <div class="housing_box">
-          <div class="housing_box_text">
+      <main className="housing">
+        <div className="housing_box">
+          <div className="housing_box_text">
             <h1>{DataHousing[0].title}</h1>
+            <p>{DataHousing[0].location}</p>
+            <div className="align">
+              {DataHousing[0].tags.map((tag, items) => {
+                return <button key={items}>{tag}</button>;
+              })}
+            </div>
+          </div>
+          <div className="alignc">
+            <div className="housing_box_text_host">
+              <div className="housing_box_text_host_name">
+                <span>{name[0]}</span>
+                <span>{name[1]}</span>
+              </div>
+              <img className='picture'src={DataHousing[0].host.picture} alt="host " />
+            </div>
+            <div className="align">
+              {[...Array(5)].map((star, item) => {
+                const ratingValue = item + 1;
+                return (
+                  <img className='stars'
+                    key={item}
+                    src={ratingValue <= rating ? staractive : starinactive}
+                    alt="star"
+                  />
+                );
+              })}
+            </div>
           </div>
         </div>
       </main>
