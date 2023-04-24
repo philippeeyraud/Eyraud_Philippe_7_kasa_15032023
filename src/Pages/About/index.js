0c9cd509f6261eeca8d04a0b008f3fd48a6dd37b
philippeeyraud <philippe.eyraud@ymail.com>
2023-04-24 18:54:11 +0200

    about

--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -3,13 +3,33 @@ import Banner from './../../components/Banner'
 import './index.scss'
 import bannerAbout from "./../../assets/bannerAbout.png";
 import Footer from './../../components/Footer'
+import listing from './../../DataAbout/index'
 import React from 'react'
+import Collapse from './../../components/Collapse/index'
+
 
 function About() {
 return (
   <div>
     <Header />
     <Banner bannerImage={bannerAbout} />;
+    <main className ='listing'>
+{listing.map(data => {
+
+  return (
+  <div key={data.id} className='listing_collapse'>
+<Collapse  title = { data.title} text = {data.description}/>
+
+  </div>
+
+
+  )}
+)}
+
+
+
+
+    </main>
     <Footer />
   </div>
 );    
