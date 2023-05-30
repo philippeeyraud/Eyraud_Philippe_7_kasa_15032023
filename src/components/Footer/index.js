56d8d72594a6f05d5750e0fbd8b46747cdabd3e7
philippeeyraud <philippe.eyraud@ymail.com>
2023-05-30 11:03:08 +0200

    scss

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,14 @@
 import "../../components/Footer/index.scss";
-import logo from "../../assets/Footer.png";
-
+import logo from "../../assets/logoF.png";
+import Logo from "../../assets/© 2020 Kasa. All rights reserved.png";
 
 function Footer() {
   return (
     <footer className="footer">
-      <img className= "height-2" src={logo} alt="location immobilier" />
+      <img className="log" src={logo} alt="location immobilier" />
+     
+      <img className="logi" src={Logo} alt="location immobilier" />
+    
     </footer>
   );
 }
