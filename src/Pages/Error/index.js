939fc93182393a9aa124983b91bde2522fca2562
philippeeyraud <philippe.eyraud@ymail.com>
2023-06-05 13:26:35 +0200

    scss

--- src/Pages/Error/index.js
+++ src/Pages/Error/index.js
@@ -1,28 +1,23 @@
-import Header from '../../components/Header'
-import {Link }from 'react-router-dom'
-import './index.scss'
-import Four from './../../assets/Four.png'
-import RetourAccueil from './../../assets/RetourAccueil.png'
-
+import Header from "../../components/Header";
+import { Link } from "react-router-dom";
+import "./index.scss";
 
 function Error() {
-    return (
-      <div>
-        <Header />
-        <div className="errorpage">
-          <div className="errorpage_news">
-            <h2>
-             404
-            </h2>
-            <p className= "texterror">
-              Oups! La page que vous demandez n'existe pas.
-            </p>
-            <Link className="returnaccueil" to="/">
-              Retourner sur la page d'accueil
-            </Link>
-          </div>
+  return (
+    <div>
+      <Header />
+      <div className="errorpage">
+        <div className="errorpage_news">
+          <h2>404</h2>
+          <p className="texterror">
+            Oups ! La page que vous demandez n'existe pas.
+          </p>
+          <Link className="returnaccueil" to="/">
+            Retourner sur la page d'accueil
+          </Link>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 export default Error;
